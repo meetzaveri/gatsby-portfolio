150f6c5e882f9848eedf04da90ad597309e53617
Meet Zaveri <meethpatel96@gmail.com>
2019-02-16 16:36:29 +0530

    [UPDATE] : Added some metadata to react helmet for SEO

--- src/components/seo.js
+++ src/components/seo.js
@@ -60,7 +60,15 @@ function SEO({ description, lang, meta, keywords, title }) {
                   : []
               )
               .concat(meta)}
-          />
+          >
+            <link rel="shortcut icon" href="/icons/profile-pic.png" />
+            <link
+              rel="stylesheet"
+              href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+              integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+              crossorigin="anonymous"
+            />
+          </Helmet>
         )
       }}
     />
